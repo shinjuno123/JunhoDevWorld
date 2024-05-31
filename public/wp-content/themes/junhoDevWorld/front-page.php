e797834664fc8585a395629413d669a1bc0eea98
junho@bobsweep.com <shinjuno123@naver.com>
2024-05-30 22:06:45 -0700

    Add intro section

--- public/wp-content/themes/junhoDevWorld/front-page.php
+++ public/wp-content/themes/junhoDevWorld/front-page.php
@@ -2,7 +2,15 @@
 
 <main class="main container">
     <div class="intro">
-        <div class="intro__inner" style="height:200vh;"></div>
+        <div class="intro__inner">
+            <h1 class="intro__title">I make websites</h1>
+            <p class="intro__pretitle">
+                <a href="/">Hello, my name is Junho</a>
+            </p>
+            <div class="intro__content">
+                <p>I’m a front-end developer and co-founder of Codista, a software studio in Vienna. I also write about the web on my blog and elsewhere.</p>
+            </div>
+        </div>
     </div>
 </main>
 
