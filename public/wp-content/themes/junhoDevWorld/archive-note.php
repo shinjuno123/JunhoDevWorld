e66d0cfc35e7b792dd61222de71d999aadaaf48a
junho@bobsweep.com <shinjuno123@naver.com>
2024-06-12 22:24:44 -0700

    On progress with notes page

--- public/wp-content/themes/junhoDevWorld/archive-note.php
+++ public/wp-content/themes/junhoDevWorld/archive-note.php
@@ -1,7 +1,9 @@
 <?php get_header(); ?>
 
 <main class="main container">
+
     <section class="section__notes">
+        <span class="notes__header">Notes</span>
         <ul class="notes">
             <li class="note">
                 <header class="note__header">
@@ -13,7 +15,7 @@
                     </div>
                 </header>
 
-                <main class="note_main">
+                <main class="note__main">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
                     et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
                     aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
@@ -23,14 +25,16 @@
 
                 <footer class="note__footer">
                     <div class="left">
+                        <i class="fa-solid fa-heart"></i>
                         <i class="fa-regular fa-heart"></i>
                         <span> You and 201 people like this</span>
                     </div>
 
                     <div class="right">
-                        <button>
-                        <i class="fa-regular fa-comment"></i>
-                        <span>20</span>
+                        <button type="button">
+                            <i class="fa-solid fa-comment"></i>
+                            <i class="fa-regular fa-comment"></i>
+                            <span>20</span>
                         </button>
                     </div>
                 </footer>
@@ -46,7 +50,7 @@
                     </div>
                 </header>
 
-                <main class="note_main">
+                <main class="note__main">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
                     et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
                     aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
@@ -62,75 +66,8 @@
 
                     <div class="right">
                         <button>
-                        <i class="fa-regular fa-comment"></i>
-                        <span>20</span>
-                        </button>
-                    </div>
-                </footer>
-            </li>
-
-
-            <li class="note">
-                <header class="note__header">
-                    <img class="note-profile__image" src="<?php echo get_template_directory_uri() ?>/images/myface.jpg"
-                        alt="">
-                    <div class="note-profile">
-                        <span class="note-profile__name">Junho Shin</span>
-                        <span class="note-profile__uploaded">20 min ago</span>
-                    </div>
-                </header>
-
-                <main class="note_main">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum.
-                </main>
-
-                <footer class="note__footer">
-                    <div class="left">
-                        <i class="fa-regular fa-heart"></i>
-                        <span> You and 201 people like this</span>
-                    </div>
-
-                    <div class="right">
-                        <button>
-                        <i class="fa-regular fa-comment"></i>
-                        <span>20</span>
-                        </button>
-                    </div>
-                </footer>
-            </li>
-
-            <li class="note">
-                <header class="note__header">
-                    <img class="note-profile__image" src="<?php echo get_template_directory_uri() ?>/images/myface.jpg"
-                        alt="">
-                    <div class="note-profile">
-                        <span class="note-profile__name">Junho Shin</span>
-                        <span class="note-profile__uploaded">20 min ago</span>
-                    </div>
-                </header>
-
-                <main class="note_main">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum.
-                </main>
-
-                <footer class="note__footer">
-                    <div class="left">
-                        <i class="fa-regular fa-heart"></i>
-                        <span> You and 201 people like this</span>
-                    </div>
-
-                    <div class="right">
-                        <button>
-                        <i class="fa-regular fa-comment"></i>
-                        <span>20</span>
+                            <i class="fa-regular fa-comment"></i>
+                            <span>20</span>
                         </button>
                     </div>
                 </footer>
@@ -147,7 +84,7 @@
                     </div>
                 </header>
 
-                <main class="note_main">
+                <main class="note__main">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
                     et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
                     aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
@@ -163,8 +100,41 @@
 
                     <div class="right">
                         <button>
-                        <i class="fa-regular fa-comment"></i>
-                        <span>20</span>
+                            <i class="fa-regular fa-comment"></i>
+                            <span>20</span>
+                        </button>
+                    </div>
+                </footer>
+            </li>
+
+            <li class="note">
+                <header class="note__header">
+                    <img class="note-profile__image" src="<?php echo get_template_directory_uri() ?>/images/myface.jpg"
+                        alt="">
+                    <div class="note-profile">
+                        <span class="note-profile__name">Junho Shin</span>
+                        <span class="note-profile__uploaded">20 min ago</span>
+                    </div>
+                </header>
+
+                <main class="note__main">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                </main>
+
+                <footer class="note__footer">
+                    <div class="left">
+                        <i class="fa-regular fa-heart"></i>
+                        <span> You and 201 people like this</span>
+                    </div>
+
+                    <div class="right">
+                        <button>
+                            <i class="fa-regular fa-comment"></i>
+                            <span>20</span>
                         </button>
                     </div>
                 </footer>
@@ -181,7 +151,7 @@
                     </div>
                 </header>
 
-                <main class="note_main">
+                <main class="note__main">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
                     et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
                     aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
@@ -197,8 +167,42 @@
 
                     <div class="right">
                         <button>
-                        <i class="fa-regular fa-comment"></i>
-                        <span>20</span>
+                            <i class="fa-regular fa-comment"></i>
+                            <span>20</span>
+                        </button>
+                    </div>
+                </footer>
+            </li>
+
+
+            <li class="note">
+                <header class="note__header">
+                    <img class="note-profile__image" src="<?php echo get_template_directory_uri() ?>/images/myface.jpg"
+                        alt="">
+                    <div class="note-profile">
+                        <span class="note-profile__name">Junho Shin</span>
+                        <span class="note-profile__uploaded">20 min ago</span>
+                    </div>
+                </header>
+
+                <main class="note__main">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                </main>
+
+                <footer class="note__footer">
+                    <div class="left">
+                        <i class="fa-regular fa-heart"></i>
+                        <span> You and 201 people like this</span>
+                    </div>
+
+                    <div class="right">
+                        <button>
+                            <i class="fa-regular fa-comment"></i>
+                            <span>20</span>
                         </button>
                     </div>
                 </footer>
